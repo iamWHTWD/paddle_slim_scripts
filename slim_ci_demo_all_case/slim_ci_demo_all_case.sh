39303233b617bc30d144e3e251f3ced19619c8be
iamWHTWD <iamwhtwd@163.com>
2021-07-30 23:50:22 +0800

    update p0 case

--- slim_ci_demo_all_case/slim_ci_demo_all_case.sh
+++ slim_ci_demo_all_case/slim_ci_demo_all_case.sh
@@ -5,7 +5,7 @@ print_info(){
 if [ $1 -ne 0 ];then
     mv ${log_path}/$2 ${log_path}/FAIL_$2.log
     echo -e "\033[31m ${log_path}/FAIL_$2 \033[0m"
-    cat FAIL_$2.log
+    cat ${log_path}/FAIL_$2.log
 else
     mv ${log_path}/$2 ${log_path}/SUCCESS_$2.log
     echo -e "\033[32m ${log_path}/SUCCESS_$2 \033[0m"
