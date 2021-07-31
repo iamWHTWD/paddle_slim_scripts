b5c4f36ff764de3c19383f3a6eda462819aa115e
iamWHTWD <iamwhtwd@163.com>
2021-07-31 09:20:35 +0800

    update case of printf method

--- slim_ci_demo_all_case/slim_ci_api_coverage.sh
+++ slim_ci_demo_all_case/slim_ci_api_coverage.sh
@@ -2,12 +2,13 @@
 
 print_info(){
 if [ $1 -ne 0 ];then
-    mv ${log_path}/$2 ${log_path}/FAIL_$2.log
-    echo -e "\033[31m ${log_path}/FAIL_$2 \033[0m"
-    cat ${log_path}/FAIL_$2.log
+    mv ${log_path}/$2 ${log_path}/$2_FAIL.log
+    echo -e "\033[31m ${log_path}/$2_FAIL \033[0m"
+    echo FAIL case log as bleow
+    cat ${log_path}/$2_FAIL.log
 else
-    mv ${log_path}/$2 ${log_path}/SUCCESS_$2.log
-    echo -e "\033[32m ${log_path}/SUCCESS_$2 \033[0m"
+    mv ${log_path}/$2 ${log_path}/$2_SUCCESS.log
+    echo -e "\033[32m ${log_path}/$2_SUCCESS \033[0m"
 fi
 }
 
--- slim_ci_demo_all_case/slim_ci_demo_all_case.sh
+++ slim_ci_demo_all_case/slim_ci_demo_all_case.sh
@@ -5,6 +5,7 @@ print_info(){
 if [ $1 -ne 0 ];then
     mv ${log_path}/$2 ${log_path}/FAIL_$2.log
     echo -e "\033[31m ${log_path}/FAIL_$2 \033[0m"
+    echo FAIL case log as bleow
     cat ${log_path}/FAIL_$2.log
 else
     mv ${log_path}/$2 ${log_path}/SUCCESS_$2.log
